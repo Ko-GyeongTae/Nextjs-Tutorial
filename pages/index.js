b193ec5f8243da2842970d0a36bd0c0e4a8dd24f
Ko-GyeongTae <kokt0203@naver.com>
2021-06-07 08:52:06 +0900

    API

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
+import axios from 'axios';
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useEffect } from 'react';
 
 export default function Home() {
+  const API_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
+
+  function getData() {
+    axios.get(API_URL)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(e => console.log(e));
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
       <Head>
-        <title>HOME | INFO</title>
+        <title>HOME | NEON</title>
       </Head>
-      create-next-ap!!! create-next-app 으로 설치하면
-      
+      create-next-app!!! create-next-app 으로 설치하면
+
     </div>
   )
 }
