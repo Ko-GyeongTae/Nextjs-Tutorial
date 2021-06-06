f5285a60466ec22c6d1d9600d050129641d6389a
Ko-GyeongTae <kokt0203@naver.com>
2021-06-06 23:32:23 +0900

    <EDIT> Files

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import 'semantic-ui-css/semantic.min.css';
 import Footer from '../src/components/Footer';
+import Top from '../src/components/Top';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Top />
       <Component {...pageProps} />
       <Footer />
     </div>
--- src/components/Top.js
+++ src/components/Top.js
@@ -1,10 +1,12 @@
 import { Header } from "semantic-ui-react"
+import Gnb from "./Gnb"
 
-export default Top = () => {
+export default function Top() {
     return (
         <div>
-            <img src={'/images/info.png'} alt='logo'/>
+            <img src={'/images/인포로고.png'} alt='logo'/>
             <Header as="h1">부산판다</Header>
+            <Gnb />
         </div>
     );
 }
