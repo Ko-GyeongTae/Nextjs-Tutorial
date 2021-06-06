dcc02709713d2502eb95da17ed1109417acd025f
Ko-GyeongTae <kokt0203@naver.com>
2021-06-07 08:26:45 +0900

    <EDIT>

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,11 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>HOME | INFO</title>
+      </Head>
       create-next-ap!!! create-next-app 으로 설치하면
-
+      
     </div>
   )
 }
--- src/components/Top.js
+++ src/components/Top.js
@@ -4,8 +4,12 @@ import Gnb from "./Gnb"
 export default function Top() {
     return (
         <div>
-            <img src={'/images/인포로고.png'} alt='logo'/>
-            <Header as="h1">부산판다</Header>
+            <div style={{ display: 'flex', paddingTop: 20 }}>
+                <div style={{ flex: '100px 0 0'}}>
+                    <img src={'/images/인포로고.png'} alt='logo' style={{ display: 'block', width: 80 }}/>
+                </div>
+                <Header as="h1">고경태</Header>
+            </div>
             <Gnb />
         </div>
     );
