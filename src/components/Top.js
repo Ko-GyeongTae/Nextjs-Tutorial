8c66da7278aeb136d2f58cf4f0e5e344de946386
Ko-GyeongTae <kokt0203@naver.com>
2021-06-06 21:39:26 +0900

    <ADD> Basic

---
+++ src/components/Top.js
@@ -0,0 +1,10 @@
+import { Header } from "semantic-ui-react"
+
+export default Top = () => {
+    return (
+        <div>
+            <img src={'/images/info.png'} alt='logo'/>
+            <Header as="h1">부산판다</Header>
+        </div>
+    );
+}
