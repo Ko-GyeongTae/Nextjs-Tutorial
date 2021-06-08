27b89b0653f649c8145a20b4ba77472c574bee6b
Ko-GyeongTae <kokt0203@naver.com>
2021-06-08 20:26:03 +0900

    <EDIT> Somethings

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -1,10 +1,31 @@
+import axios from 'axios';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Item from '../../src/components/Item';
 
 const Post = () => {
     const router = useRouter();
     const { id } = router.query;
 
-    return <p>Post: {id}</p>;
+    const [item, setItem] = useState({});
+
+    const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+
+    function getData() {
+        axios.get(API_URL)
+        .then(res => {
+            console.log(res.data);
+            setItem(res.data);
+        })
+    };
+
+    useEffect(() => {
+        if(id && id > 0){
+            getData();
+        }
+    }, [id]);
+
+    return <Item item={item} />;
 };
 
 export default Post;
