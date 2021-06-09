be14abc249f3076840cc1b72a6ef2b7360ba994b
Ko-GyeongTae <kokt0203@naver.com>
2021-06-09 13:31:39 +0900

    <ADD> Router

--- src/components/Gnb.js
+++ src/components/Gnb.js
@@ -1,23 +1,28 @@
 import { Menu } from "semantic-ui-react";
+import { useRouter } from 'next/router';
 
 export default function Gnb(){
     const activeItem = 'home';
+    const router = useRouter();
+
+    function goLink(e, data){
+        if(data.name === 'home'){
+            router.push('/');
+        } else if(data.name === 'about'){
+            router.push('/about');
+        }
+    }
     return (
         <Menu inverted>
             <Menu.Item
                 name="home"
                 active={activeItem === 'home'}
-                //onClick={this.handleItemClick}
+                onClick={goLink}
             />
             <Menu.Item
-                name="messages"
-                active={activeItem === 'messages'}
-                //onClick={this.handleItemClick}
-            />
-            <Menu.Item
-                name="friends"
-                active={activeItem === 'friends'}
-                //onClick={this.handleItemClick}
+                name="about"
+                active={activeItem === 'about'}
+                onClick={goLink}
             />
         </Menu>
     );
